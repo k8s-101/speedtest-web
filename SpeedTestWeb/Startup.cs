3d67c5e0a6baead975ee3244bd36283071b18448
teodoran <teodor@andeelstad.com>
2019-04-27 08:59:30 +0200

    .NET static host for js-client

---
+++ SpeedTestWeb/Startup.cs
@@ -0,0 +1,47 @@
+using System.IO;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
+
+namespace SpeedTestWeb
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDirectoryBrowser();
+        }
+
+        public void Configure(IApplicationBuilder application, IHostingEnvironment environment)
+        {
+            if (environment.IsDevelopment())
+            {
+                application.UseDeveloperExceptionPage();
+            }
+
+            application.UseDefaultFiles();
+
+            var clientPath = Path.Combine(Directory.GetCurrentDirectory(), "Client");
+            application.UseStaticFiles(new StaticFileOptions
+            {
+                FileProvider = new PhysicalFileProvider(clientPath),
+                RequestPath = ""
+            });
+
+            application.UseDirectoryBrowser(new DirectoryBrowserOptions
+            {
+                FileProvider = new PhysicalFileProvider(clientPath),
+                RequestPath = ""
+            });
+        }
+    }
+}
